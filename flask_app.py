9c90939fc6e083c9086682f01bf935dbc935b37f
Starelie <timotheelaberge@gmail.com>
2026-04-16 09:48:19 -0400

    Replace `'` string with `"`

--- flask_app.py
+++ flask_app.py
@@ -4,14 +4,14 @@ from flask import Flask, flash, request, redirect, url_for, render_template
 from werkzeug.utils import secure_filename
 
 # Declare constants
-UPLOAD_FOLDER = 'uploads/'
+UPLOAD_FOLDER = "uploads/"
 DATABASE_FOLDER = "databases/"
 TEMPLATE_FOLDER = "templates.folder/"
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+ALLOWED_EXTENSIONS = {"txt", "pdf", "png", "jpg", "jpeg", "gif"}
 
 # Setup flask
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 app.template_folder = TEMPLATE_FOLDER
 
 # Create necessary folders if they don't already exist
@@ -26,12 +26,12 @@ cursor.close()
 connection.close()
 
 def allowed_file(filename: str) -> bool:
-  return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+  return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
 def add_file_database(filename: str) -> None:
   connection = sqlite3.connect(f"{DATABASE_FOLDER}/uploads.db")
   cursor = connection.cursor()
-  table_entry = filename.rsplit('.', 1)
+  table_entry = filename.rsplit(".", 1)
   files = cursor.execute("SELECT filename, extension FROM uploads").fetchall()
   for (name, extension) in files:
     if name == table_entry[0] and extension == table_entry[1]:
@@ -49,24 +49,24 @@ def clean_filename(filename: str) -> str:
   clean_filename = split_filename[0] + "." + split_filename[1].lower()
   return clean_filename
 
-@app.route('/')
+@app.route("/")
 def home():
   return render_template("base.html")
 
-@app.route("/upload", methods=['GET', 'POST'])
+@app.route("/upload", methods=["GET", "POST"])
 def upload_file():
-  if request.method == 'POST':
+  if request.method == "POST":
     # check if the post request has the file part
-    if 'file' not in request.files:
-      flash('No file part')
+    if "file" not in request.files:
+      flash("No file part")
       return redirect(request.url)
-    file = request.files['file']
+    file = request.files["file"]
     
     if file and allowed_file(file.filename):
       filename = clean_filename(secure_filename(file.filename))
       add_file_database(filename)
-      file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-      return redirect(url_for('download_file', name=filename))
+      file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+      return redirect(url_for("download_file", name=filename))
   return render_template("upload.html")
 
 @app.route("/download")
