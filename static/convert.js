511e350b0c89d63f34d067c520f687c5120d16ad
Starelie <timotheelaberge@gmail.com>
2026-04-19 23:27:34 -0400

    Add comments to 'convert.js'

--- static/convert.js
+++ static/convert.js
@@ -2,26 +2,30 @@ COLUMN_NUMBER = 3
 
 function file_selection()
 {
+  // trouver la liste de fichiers
   let list = document.getElementById("files-list");
   let list_elements = list.children;
+  // si la colonne "Sélection" est cliquée
   for (let i = COLUMN_NUMBER + 1; i < list_elements.length; i += COLUMN_NUMBER) {
     list_elements[i].addEventListener("click", () => {
+      // échanger entre "choisit" et "choisir" et enlever "choisit" des autres rangées
       if (!list_elements[i].classList.contains("selected")) {
         for (let j = COLUMN_NUMBER + 1; j < list_elements.length; j += COLUMN_NUMBER) {
           if (list_elements[j].classList.contains("selected")) {
-            list_elements[j].firstChild.textContent = "select";
+            list_elements[j].firstChild.textContent = "choisir";
             list_elements[j].lastChild.textContent = "☐";
             list_elements[j].classList.remove("selected");
           }
         }
-        list_elements[i].firstChild.textContent = "selected";
+        list_elements[i].firstChild.textContent = "choisit";
         list_elements[i].lastChild.textContent = "☑";
         list_elements[i].classList.add("selected");
 
+        // appeler select_extension pour que la bonne extension soit choisie
         select_extension(list_elements[i + 1])
       }
       else {
-        list_elements[i].firstChild.textContent = "select";
+        list_elements[i].firstChild.textContent = "choisir";
         list_elements[i].lastChild.textContent = "☐";
         list_elements[i].classList.remove("selected");
       }
@@ -33,14 +37,17 @@ function file_selection()
 
 function select_extension(element)
 {
+  // ajouter la valeur choisie dans le formulaire
   let selected_value = element.options[element.selectedIndex].value;
   document.getElementById("selected-extension").value = selected_value;
 }
 
 function extension_selection()
 {
+  // trouver la liste de fichiers
   let list = document.getElementById("files-list");
   let list_elements = list.children;
+  // quand le sélecteur change, appeler select_extension
   for (let i = COLUMN_NUMBER + 2; i < list_elements.length; i += COLUMN_NUMBER) {
     list_elements[i].addEventListener("change", (element) => {
       if (list_elements[i - 1].classList.contains("selected"))
@@ -53,6 +60,7 @@ function extension_selection()
 
 function get_selected_file()
 {
+  // trouver quel fichier est choisit
   let list = document.getElementById("files-list");
   let list_elements = list.children;
   for (let i = COLUMN_NUMBER + 1; i < list_elements.length; i += COLUMN_NUMBER) {
@@ -63,5 +71,6 @@ function get_selected_file()
   return;
 }
 
+// appeler les fonctions nécessaires
 file_selection()
 extension_selection()
